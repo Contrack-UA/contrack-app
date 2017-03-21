ba1e2540be730c4c5c86c7917db7a962122809e6
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-03-21 13:20:03 -0500

    Fix indentation

--- imports/ui/Navbar.jsx
+++ imports/ui/Navbar.jsx
@@ -3,26 +3,27 @@ import React, {Component, PropTypes} from 'react';
 import Accounts from './AccountsUIWrapper.jsx';
 
 export default class Navbar extends Component {
-    render() {
-        return (
-            <div>
-                <div className="navbar navbar-default" role="navigation">
-                    <div className="navbar-header">
-                        <a className="navbar-brand" href="#">Contrack</a>
-                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-                            <span className="sr-only">Toggle navigation</span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                        </button>
-                    </div>
-                    <div className="navbar-collapse collapse">
-                        <ul className="nav navbar-nav navbar-right">
-                            <Accounts/>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+
+  render() {
+    return (
+      <div>
+        <div className="navbar navbar-default" role="navigation">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="#">Contrack</a>
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+          </div>
+          <div className="navbar-collapse collapse">
+            <ul className="nav navbar-nav navbar-right">
+              <Accounts/>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
