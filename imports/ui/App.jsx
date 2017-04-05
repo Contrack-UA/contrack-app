478aa75b9795b5e6bbbc9a7ef9d9cd8a97893dd0
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2017-04-04 22:20:41 -0500

    App update

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
-import HomePage from './HomePage';
 import Navigbar from './Navigbar';
-import Projects from './Projects.jsx';
-import Invoices from './Invoices.jsx';
-
-import axios from 'axios';
 
 // App component - represents the whole app
 export default class App extends Component {
@@ -14,84 +9,23 @@ export default class App extends Component {
     this.state = {
       status: 'home',
       page:1,
-      pageL:[],
       currentProject: 'prueba'
-    } // Cambiar status a invoices para ver las facturas
-    this.goProjects = this.goProjects.bind(this);
-    this.changePage = this.changePage.bind(this);
-    this.goInvoices = this.goInvoices.bind(this);
-    this.goHome = this.goHome.bind(this);
-  }
-
-  retrievePage(i){
-    axios.get("http://datos.colombiacompra.gov.co:8000/releases/contract/?page="+i, {
-    }).then(response => {
-        console.log(response);
-        if (response.data === null) {
-            alert("no hay contratos para esta pagina");
-        }
-        else
-        {
-            //los proyectos estan en releases
-            var list = response.data.releases;
-            this.setState({pageL:list,page:i});
-        }
-    });
-  }
-
-  renderComponent() {
-    if(this.state.status === 'home') {
-      return (
-        <HomePage goProjects={this.goProjects} />
-      );
-    } else if (this.state.status === 'invoices') {
-      return this.renderInvoices(this.state.currentProject);
-    } else if (this.state.status === 'projects') {
-      return (
-        <Projects page={this.state.page}
-          lista={this.state.pageL} changePage={this.changePage}
-          goInvoices={this.goInvoices}
-          goHome={this.goHome}/>
-      );
-    } else {
-      return (
-        <p>Hola</p>
-      )
     }
-  }
-
-  goProjects() {
-    console.log("GO PROJECTS");
-    this.setState({status:'projects'});
-  }
-  goHome() {
-    this.setState({status:'home'});
-  }
-  changePage(i){
-    this.retrievePage(i);
-  }
-  goInvoices(project){
-    console.log("llega el llamado invoices");
-    this.setState({
-      status:'invoices',
-      currentProject: project
-    });
-  }
-  renderInvoices(project) {
-    console.log("llega a crear invoices");
-    return (
-      <Invoices project={project} goHome={this.goHome}/>
-    )
   }
 
   render() {
-    if(this.state.pageL.length === 0)
-    {
-      this.retrievePage(1);
-    }
     return (
       <div className="container">
-        {this.renderComponent()}
+        <div className="row">
+          <Navigbar />
+        </div>
+        <div className="row">
+          <div className='col-md-1'></div>
+          <div className='col-md-10'>
+            {React.cloneElement(this.props.children, {...this.state})}
+          </div>
+          <div className='col-md-1'></div>
+        </div>
       </div>
     );
   }
---
+++ imports/ui/Appx.jsx
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import HomePage from './HomePage';
+import Navigbar from './Navigbar';
+import Projects from './Projects.jsx';
+import Invoices from './Invoices.jsx';
+
+
+
+// App component - represents the whole app
+export default class Appx extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: 'home',
+      page:1,
+      pageL:[],
+      currentProject: 'prueba'
+    } // Cambiar status a invoices para ver las facturas
+    this.goProjects = this.goProjects.bind(this);
+    this.changePage = this.changePage.bind(this);
+    this.goInvoices = this.goInvoices.bind(this);
+    this.goHome = this.goHome.bind(this);
+  }
+
+  retrievePage(i){
+    axios.get("http://datos.colombiacompra.gov.co:8000/releases/contract/?page="+i, {
+    }).then(response => {
+        console.log(response);
+        if (response.data === null) {
+            alert("no hay contratos para esta pagina");
+        }
+        else
+        {
+            //los proyectos estan en releases
+            var list = response.data.releases;
+            this.setState({pageL:list,page:i});
+        }
+    });
+  }
+
+  renderComponent() {
+    if(this.state.status === 'home') {
+      return (
+        <HomePage goProjects={this.goProjects} />
+      );
+    } else if (this.state.status === 'invoices') {
+      return this.renderInvoices(this.state.currentProject);
+    } else if (this.state.status === 'projects') {
+      return (
+        <Projects page={this.state.page}
+          lista={this.state.pageL} changePage={this.changePage}
+          goHome={this.goHome}/>
+      );
+    } else {
+      return (
+        <p>Hola</p>
+      )
+    }
+  }
+
+  goProjects() {
+    console.log("GO PROJECTS");
+    this.setState({status:'projects'});
+  }
+  goHome() {
+    this.setState({status:'home'});
+  }
+  changePage(i){
+    this.retrievePage(i);
+  }
+  goInvoices(project){
+    console.log("llega el llamado invoices");
+    this.setState({
+      status:'invoices',
+      currentProject: project
+    });
+  }
+  renderInvoices(project) {
+    console.log("llega a crear invoices");
+    return (
+      <Invoices project={project} goHome={this.goHome}/>
+    )
+  }
+
+  render() {
+    if(this.state.pageL.length === 0)
+    {
+      this.retrievePage(1);
+    }
+    return (
+      <div className="container">
+        {this.renderComponent()}
+      </div>
+    );
+  }
+}
