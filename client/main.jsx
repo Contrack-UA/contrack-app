45727bf04dc4b4f98b347756a1622efd9bbce5d5
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2017-04-04 21:00:16 -0500

    react-router attempt

--- client/main.jsx
+++ client/main.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import {browserHistory, Router, Route, IndexRoute} from 'react-router';
 
-import '../imports/startup/accounts-config.js';
 import App from '../imports/ui/App.jsx';
-
+import HomePage from '../imports/ui/HomePage.jsx';
+import Projects from '../imports/ui/Projects.jsx';
 
 Meteor.startup(() => {
-  render(<App />, document.getElementById('render-target'));
+  render(
+  <Router history={browserHistory}>
+    <Route path='/' component={App}>
+      <IndexRoute component={HomePage} />
+      <Route path= 'contratos' component={Projects}/>
+    </Route>
+  </Router>, document.getElementById('render-target')
+);
 });
