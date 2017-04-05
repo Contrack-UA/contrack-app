924eae339f1522bc9d5e592de549db1d49f3447c
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2017-04-05 00:48:35 -0500

    Homepage improvements

--- imports/ui/HomePage.jsx
+++ imports/ui/HomePage.jsx
@@ -1,17 +1,55 @@
 import React, {Component, PropTypes} from 'react';
-import {Template} from 'meteor/templating';
+import {Well} from 'react-bootstrap';
+
 
 export default class HomePage extends Component {
 
     render() {
         return (
             <div>
-                <div className="row head center">
-                  <h1>
-                    Bienvenido a 
-                  </h1>
+              {new WOW().init()}
+                <div className="row head bold center">
+                  <h2>
+                    Bienvenido a <br/> <br/>
                     <img src="https://68.media.tumblr.com/7fad5ad226733c8604e9aa92e033b017/tumblr_onw1hqFxyO1w7ypfio1_1280.png" className="logo" />
-
+                    <br/>Una plataforma de uso libre para todos los colombianos.
+                  </h2>
+                </div>
+                <br/>
+                <div className="row bod">
+                  <div className="col-md-4">
+                    <Well>
+                      <h3 className="bold head center">Título</h3>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                         dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                         mollit anim id est laborum.</p>
+                    </Well>
+                  </div>
+                  <div className="col-md-4">
+                    <Well>
+                      <h3 className="bold head center">Título</h3>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                         dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                         mollit anim id est laborum.</p>
+                    </Well>
+                  </div>
+                  <div className="col-md-4">
+                    <Well>
+                      <h3 className="bold head center">Título</h3>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                         dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                         mollit anim id est laborum.</p>
+                    </Well>
+                  </div>
                 </div>
             </div>
         );
