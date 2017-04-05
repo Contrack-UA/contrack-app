01cd8b2153c26bb59fa6b503487e29ec32329a63
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2017-04-04 22:22:35 -0500

    Transferred functions from App to Projects

--- imports/ui/Projects.jsx
+++ imports/ui/Projects.jsx
@@ -5,18 +5,41 @@ import { createContainer } from 'meteor/react-meteor-data';
 import Matriz from './Matriz.jsx';
 import Footer from './Footer.jsx';
 import {FormGroup, FormControl, ControlLabel} from 'react-bootstrap';
+import axios from 'axios';
 
 // Project component
 export default class Projects extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchValue:''
+      searchValue:'',
+      page:1,
+      pageL:[]
+    }
+    if(this.state.pageL.length === 0)
+    {
+      this.retrievePage(1);
     }
   }
 
+  //Search form change handler
   handleChange(e){
     this.setState({searchValue: e.target.value});
+  }
+
+  //Page retriever
+  retrievePage(i){
+    axios.get("http://datos.colombiacompra.gov.co:8000/releases/contract/?page="+i, {
+    }).then(response => {
+        console.log(response);
+        if (response.data === null) {
+            alert("no hay contratos para esta pagina");
+        }else{
+            //los proyectos estan en releases
+            var list = response.data.releases;
+            this.setState({pageL:list,page:i});
+        }
+    });
   }
 
   render() {
@@ -26,9 +49,6 @@ export default class Projects extends Component {
       <br/>
         <div className="row">
           <div className="col-md-1"></div>
-          <div className="col-md-1">
-            <i className="fa fa-home fa-3x" onClick={()=>{this.props.goHome()}}></i>
-          </div>
           <div className="col-md-10">
             <form>
               <FormGroup controlId="formBasicText">
@@ -41,11 +61,11 @@ export default class Projects extends Component {
         <br/>
         <div className="row" id="matrizComponent">
           <div className="col-md-12" id='jumbo'>
-          <Matriz lista={this.props.lista} goInvoices={this.props.goInvoices} />
+          <Matriz lista={this.state.pageL} />
           </div>
         </div>
         <br/><br/>
-        <Footer page={this.props.page} changePage={this.props.changePage} />
+        <Footer page={this.state.page} changePage={this.retrievePage.bind(this)} />
       </div>
     );
   }
